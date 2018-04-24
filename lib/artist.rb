b2c394fe2d945add3b542cd6c5ee2d7b65e6c1be
Everett <everett.bowline@gmail.com>
2018-04-24 12:33:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,11 +4,22 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
   end
   
   def add_song(song)
     @songs << song
     song.artist = self
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def genres
+    @songs.each do | song |
+      @genres << song.genre
+    end
   end
   
 end
