a968be6a669a575a013149f149ee70931c90efe0
Everett <everett.bowline@gmail.com>
2018-04-24 13:00:48 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment
   attr_accessor :date, :doctor
   
-  def initiialize(date, doctor)
+  def initialize(date, doctor)
     @date = date
     @doctor = doctor
     doctor.add_appointment(self)
